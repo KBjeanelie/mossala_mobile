4e09cc1c6e1312e326e6cab5086b0b12d9688a4b
Jean-Elie <jeaneliekubembula@gmail.com>
2025-03-06 21:23:45 +0100

    Fin de la premier partie de l'ecrans des projets

--- lib/screen/main/user_projet_screen.dart
+++ lib/screen/main/user_projet_screen.dart
@@ -1,22 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mossala_mobile/widgets/app_bar.dart';
 
 import '../../core/theme/app_colors.dart';
 import '../../widgets/widgets.dart';
 
-class UserProjetScreen extends StatefulWidget {
+class UserProjetScreen extends StatelessWidget {
   const UserProjetScreen({super.key});
 
   @override
-  State<UserProjetScreen> createState() => _UserProjetScreenState();
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.textDark,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+          ),
+          title: headingTextApp("Mes Projets", color: AppColors.backgroundLight),
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 15),
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                color: AppColors.backgroundLight,
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Center(
+                child: Icon(Icons.add, size: 30, color: AppColors.textDark,),
+              ),
+            )
+          ],
+          bottom: TabBar(
+            dividerHeight: 0.0,
+            indicatorColor: AppColors.primary,
+            labelStyle: GoogleFonts.poppins(
+              fontSize: 16,
+              color: AppColors.primary,
+              fontWeight: FontWeight.w600
+            ),
+            tabs: [
+              Tab(text: "Projets remportés",),
+              Tab(text: "Projets créés",)
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            TabViewOne(),
+            TabViewTwo()
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _UserProjetScreenState extends State<UserProjetScreen> {
+
+class TabViewOne extends StatelessWidget {
+  const TabViewOne({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarWidget("Mes Projets"),
-      body: Center(child: normalTextApp("Projet")),
-    );
+    return Center(child: normalTextApp("Aucun projet remportés trouvé"));
+  }
+}
+
+class TabViewTwo extends StatelessWidget {
+  const TabViewTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: normalTextApp("Aucun projet créés trouvé"));
   }
 }
